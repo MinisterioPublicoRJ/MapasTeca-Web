310ccabff19cbb6cc9c107ec03cccc48dcf9cf52
Layla <laylacsa@msn.com>
2021-04-15 20:23:05 -0300

    Better docs

--- src/lib/molecules/listCard/ListCard.jsx
+++ src/lib/molecules/listCard/ListCard.jsx
@@ -6,6 +6,13 @@ import { listCardMain, listCardRight, listCardLeft } from './ListCard.module.css
 import { useTheme } from '../../theming';
 import { IconBadge } from '../../atoms';
 
+/**
+ * Um card genérico para listas, utiliza o átomo `IconBadge`para os detalhes do lado esquerdo.
+ Apenas o conteúdo é necessário, e você pode usar qualquer combinação de outras props.
+ Sempre ocupa toda a largura disponível no pai, e cresce sua altura para que todos os filhos caibam.
+ Não renderiza o detalhe caos não receba uma cor para ele (`detailColor`). Veja `IconBadge` para mais
+ informações sobre a aba de detalhe.
+ */
 function ListCard({ title, content, actionText, actionLink, fillColor, icon, detailColor }) {
   const { primaryFontColor, mainFontFamily, smallFontSize, primaryLinkColor } = useTheme();
 
