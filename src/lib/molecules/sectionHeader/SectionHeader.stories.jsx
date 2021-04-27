899763faf286f0cd238c612c9c17ff32c4c8358d
Layla <laylacsa@msn.com>
2021-04-27 17:22:33 -0300

    Section Header Complete

--- src/lib/molecules/sectionHeader/SectionHeader.stories.jsx
+++ src/lib/molecules/sectionHeader/SectionHeader.stories.jsx
@@ -5,6 +5,31 @@ import SectionHeader from './SectionHeader';
 export default {
   title: 'Moléculas/SectionHeader',
   component: SectionHeader,
+  decorators: [
+    (Story) => (
+      <div style={{ width: '50%', padding: '20px', backgroundColor: '#F8F9FB' }}>
+        <Story />
+      </div>
+    ),
+  ],
+};
+const Template = (args) => <SectionHeader {...args} />;
+
+export const Complete = Template.bind({});
+Complete.args = {
+  title: 'Título que não respeita casing',
+  count: 20,
+  subtitle: 'subtítulo que não respeita casing',
 };
 
-export const primary = () => <SectionHeader />
+export const ComSubtitulo = Template.bind({});
+ComSubtitulo.args = {
+  title: 'Seu título',
+  subtitle: 'subtítulo',
+};
+
+export const ComDetalhe = Template.bind({});
+ComDetalhe.args = {
+  title: 'Seu título',
+  count: '20',
+};
