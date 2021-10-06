72a9f9fdc5cd79d3835a80a1c75b1cf32c193b3a
Dione Martins <profissionaldione@gmail.com>
2021-10-06 16:15:39 -0300

    Fixed the export

--- src/lib/organisms/index.js
+++ src/lib/organisms/index.js
@@ -1,3 +1,3 @@
 /* eslint-disable-next-line */
 export { default as SearchBox } from './searchBox/SearchBox';
-export { default as SearchFilters } from './searchFilters/searchFilters';
+export { default as SearchFilters } from './searchFilters/SearchFilters';
